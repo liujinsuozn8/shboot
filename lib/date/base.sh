bf90dd1416e8ba6da93028e342532bd74fba557f
liujinsuo <710419489@qq.com>
2021-09-06 17:59:32 +0800

    update for mac

--- lib/date/base.sh
+++ lib/date/base.sh
@@ -1,61 +1,179 @@
 import string/regex
 
-Date::ToShellDateFormat(){
-  # Usage Date::ToShellDateFormat 'yyyy/MM/dd'
-  # yyyy/MM/dd ---> %Y%m%d
-  local format="$1"
+if [ "$(uname)" == 'Darwin' ]; then
+  # for Mac
+  Date::ToShellDateFormat(){
+    # Usage Date::ToShellDateFormat 'yyyy/MM/dd'
+    # yyyy/MM/dd ---> %Y%m%d
+    local format="$1"
 
-  # y year, yyyy=%Y, yy=%y
-  format=${format//yyyy/%Y}
-  format=${format//yy/%y}
+    # y year, yyyy=%Y, yy=%y
+    format=${format//yyyy/%Y}
+    format=${format//yy/%y}
 
-  # M month MM=%m
-  format=${format//MM/%m}
+    # M month MM=%m
+    format=${format//MM/%m}
 
-  # d day dd=%d
-  format=${format//dd/%d}
+    # d day dd=%d
+    format=${format//dd/%d}
 
-  # h hour (1~12) hh=%l
-  format=${format//hh/%l}
+    # h hour (1~12) hh=%l
+    format=${format//hh/%l}
 
-  # H hour (0~23) HH=%H
-  format=${format//HH/%H}
+    # H hour (0~23) HH=%H
+    format=${format//HH/%H}
 
-  # m minute mm=%M
-  format=${format//mm/%M}
+    # m minute mm=%M
+    format=${format//mm/%M}
 
-  # s second ss=%S
-  format=${format//ss/%S}
+    # s second ss=%S
+    format=${format//ss/%S}
 
-  # S milli second --> handler by Date::Format
-  
-  # F day of week %w
-  format=${format//F/%w}
-  
-  # E week of year    len=1   %A
-  format=${format//E/%A}
-  
-  # D day of year  %j
-  format=${format//D/%j}
+    # S milli second --> handler by Date::Format
+    
+    # F day of week %w
+    format=${format//F/%w}
+    
+    # E week of year    len=1   %A
+    format=${format//E/%A}
+    
+    # D day of year  %j
+    format=${format//D/%j}
 
-  # w week of year %U
-  format=${format//w/%U}
+    # w week of year %U
+    format=${format//w/%U}
 
-  # a AM./PM. %P
-  format=${format//a/%P}
+    # a AM./PM. %P
+    format=${format//a/%p}
 
-  # z zone %Z 
-  format=${format//z/%Z}
+    # z zone %Z 
+    format=${format//z/%Z}
 
-  echo "$format"
-}
-export -f Date::ToShellDateFormat
+    echo "$format"
+  }
+  export -f Date::ToShellDateFormat
 
-Date::NowTimestamp(){
-  # Usage: Date::NowTimestamp
-  date '+%s%N'
-}
-export -f Date::NowTimestamp
+  Date::NowTimestamp(){
+    # Usage: Date::NowTimestamp
+    # can not get nanoseconds in mac
+    date '+%s000000000'
+  }
+  export -f Date::NowTimestamp
+
+  Date::ZeroAMTimestramp(){
+    # Usage: Date::ZeroAMTimestramp
+    date -j -f "%Y%m%d%H%M%S" "$(date +%Y%m%d)000000" +%s
+  }
+  export -f Date::ZeroAMTimestramp
+
+  Date::Format(){
+    # Usage Date::Format timestamp format
+    # need handler SSS !!!
+    local seconds="${1:0:10}"
+    local nano="${1:10}"
+    local format=$(Date::ToShellDateFormat "$2")
+    
+    # Format for Mac:second --> result
+    local result=$( date -r "$seconds" '+'"$format")
+
+    # handler SSS
+    local nanoStr=$(Regex::Matcher "$result" '(S+)' 1)
+    if [ ! -z "$nanoStr" ];then
+      local nanoNum=${nano:0:${#nanoStr}}
+      result="${result/$nanoStr/$nanoNum}"
+    fi
+
+    echo "$result"
+  }
+  export -f Date::Format
+else
+  # for Linux
+
+  Date::ToShellDateFormat(){
+    # Usage Date::ToShellDateFormat 'yyyy/MM/dd'
+    # yyyy/MM/dd ---> %Y%m%d
+    local format="$1"
+
+    # y year, yyyy=%Y, yy=%y
+    format=${format//yyyy/%Y}
+    format=${format//yy/%y}
+
+    # M month MM=%m
+    format=${format//MM/%m}
+
+    # d day dd=%d
+    format=${format//dd/%d}
+
+    # h hour (1~12) hh=%l
+    format=${format//hh/%l}
+
+    # H hour (0~23) HH=%H
+    format=${format//HH/%H}
+
+    # m minute mm=%M
+    format=${format//mm/%M}
+
+    # s second ss=%S
+    format=${format//ss/%S}
+
+    # S milli second --> handler by Date::Format
+    
+    # F day of week %w
+    format=${format//F/%w}
+    
+    # E week of year    len=1   %A
+    format=${format//E/%A}
+    
+    # D day of year  %j
+    format=${format//D/%j}
+
+    # w week of year %U
+    format=${format//w/%U}
+
+    # a AM./PM. %P
+    format=${format//a/%P}
+
+    # z zone %Z 
+    format=${format//z/%Z}
+
+    echo "$format"
+  }
+  export -f Date::ToShellDateFormat
+
+  Date::NowTimestamp(){
+    # Usage: Date::NowTimestamp
+    date '+%s%N'
+  }
+  export -f Date::NowTimestamp
+
+  Date::ZeroAMTimestramp(){
+    # Usage: Date::ZeroAMTimestramp
+    date -d "$(date +%F)" +%s
+  }
+  export -f Date::ZeroAMTimestramp
+
+  Date::Format(){
+    # Usage Date::Format timestamp format
+    # need handler SSS !!!
+    local seconds="${1:0:10}"
+    local nano="${1:10}"
+    local format=$(Date::ToShellDateFormat "$2")
+    
+    # Format for Linux:second --> result
+    local result=$( date -d @"$seconds" '+'"$format")
+
+    # handler SSS
+    local nanoStr=$(Regex::Matcher "$result" '(S+)' 1)
+    if [ ! -z "$nanoStr" ];then
+      local nanoNum=${nano:0:${#nanoStr}}
+      result="${result/$nanoStr/$nanoNum}"
+    fi
+
+    echo "$result"
+  }
+  export -f Date::Format
+fi
+
 
 Date::FormatNow() {
   # Usage Date::FormatNow foramt
@@ -63,29 +181,3 @@ Date::FormatNow() {
   Date::Format "$timestamp" "$1"
 }
 export -f Date::FormatNow
-
-Date::Format(){
-  # Usage Date::Format timestamp format
-  # need handler SSS !!!
-  local seconds="${1:0:10}"
-  local nano="${1:10}"
-  local format=$(Date::ToShellDateFormat "$2")
-  
-  local result=$( date -d @"$seconds" '+'"$format")
-
-  # handler SSS
-  local nanoStr=$(Regex::Matcher "$result" '(S+)' 1)
-  if [ ! -z "$nanoStr" ];then
-    local nanoNum=${nano:0:${#nanoStr}}
-    result="${result/$nanoStr/$nanoNum}"
-  fi
-
-  echo "$result"
-}
-export -f Date::Format
-
-Date::ZeroAMTimestramp(){
-  # Usage: Date::ZeroAMTimestramp
-  date -d "$(date +%F)" +%s
-}
-export -f Date::ZeroAMTimestramp
