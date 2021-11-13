e392cfe7a55ab1e427f8a760041be5a782df2b5e
liujinsuo <710419489@qq.com>
2021-11-14 07:29:29 +0800

    update boot

---
+++ boot.sh
@@ -0,0 +1,8 @@
+
+#---------------------------------------
+# https://github.com/liujinsuozn8/shboot
+# LICENSE: MIT License
+#---------------------------------------
+
+# Load system
+source "$( cd ${BASH_SOURCE[0]%/*} && pwd )/lib/system/load.sh"
