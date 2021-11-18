298c05ff77245dbeb295c31cb3dd0046e7379425
liujinsuo <710419489@qq.com>
2021-11-18 21:16:23 +0800

    update exception

--- lib/system/exception/exception.sh
+++ lib/system/exception/exception.sh
@@ -12,7 +12,7 @@
 #   fi
 #   ((___in_try_catch___+=1))
 #   export ___setOps___=$(echo $-); set +e; (set -e; trap __saveGlobalVar EXIT INT TERM HUP QUIT ABRT;'
-alias try='[ -z "$___in_try_catch___" ] && export ___in_try_catch___=0 && __init_exception_cache; ((___in_try_catch___+=1)); export ___setOps___=$(echo $-); set +e; (set -e; trap __saveGlobalVar EXIT INT TERM HUP QUIT ABRT; exec 2> "$__boot_exception_cache";'
+alias try='[ -z "$___in_try_catch___" ] && export ___in_try_catch___=0 && __init_exception_cache; ((___in_try_catch___+=1)); export ___setOps___=$(echo $-); set +e; (set -e; trap __saveGlobalVar EXIT INT ABRT; exec 2> "$__boot_exception_cache";'
 
 alias catch='); ___exitCode___=$?; [ $___exitCode___ -ne 0 ] && ___EXCEPTION___=$(Exception::GetException $___exitCode___); ((___in_try_catch___-=1)); [ $___in_try_catch___ -eq 0 ] && unset ___in_try_catch___; __recoverGlobalVar; set -"$___setOps___"; [ $___exitCode___ -eq 0 ] && unset ___exitCode___ ||'
 # alias catch='); ___exitCode___=$?; [ $___exitCode___ -ne 0 ] && ___EXCEPTION___=$(Exception::GetException $___exitCode___); ((___in_try_catch___-=1)); [ $___in_try_catch___ -eq 0 ] && unset ___in_try_catch___; __recoverGlobalVar; set -"$___setOps___"; [ $___exitCode___ -eq 0 ] && unset ___exitCode___ ||'
