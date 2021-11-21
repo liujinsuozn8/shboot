acfe00f9c880c843a6141a28c35dcb14d3737a18
liujinsuo <710419489@qq.com>
2021-11-21 22:30:59 +0800

    update exception: show line number when use exit

--- lib/system/keyword/exception.sh
+++ lib/system/keyword/exception.sh
@@ -12,42 +12,19 @@
 #   fi
 #   ((___in_try_catch___+=1))
 #   export ___setOps___=$(echo $-); set +e; (set -e; trap __saveGlobalVar EXIT INT TERM HUP QUIT ABRT;'
-alias try='[ -z "$___in_try_catch___" ] && export ___in_try_catch___=0 && __init_exception_cache; ((___in_try_catch___+=1)); export ___setOps___="$-"; set +e; (set -e; addTrap "__saveGlobalVar" EXIT INT ABRT; exec 2> "$__boot_exception_cache";'
+alias try='[ -z "$___in_try_catch___" ] && export ___in_try_catch___=0 && __init_exception_cache; ((___in_try_catch___+=1)); export ___setOps___="$-"; set +e; (set -e; addTrap "__getErrorInfoInTrap" EXIT; addTrap "__saveGlobalVar" EXIT INT ABRT; exec 2> "$__boot_exception_cache";'
 
 alias catch='); ___exitCode___=$?; [ $___exitCode___ -ne 0 ] && ___EXCEPTION___=$(Exception::GetException $___exitCode___); ((___in_try_catch___-=1)); [ $___in_try_catch___ -eq 0 ] && unset ___in_try_catch___; __recoverGlobalVar; set -"$___setOps___"; [ $___exitCode___ -eq 0 ] && unset ___exitCode___ ||'
 
 printStackTrace(){
   # Usage: printStackTrace "$___EXCEPTION___"
-
   if declare -f "Log::ERROR" &> /dev/null; then
     Log::ERROR "\n$1"
   else
-    echo -en "\033[0;31m$1\n\033[0m" 1>&2
+    echo -en "\033[0;31m$1\n\033[0m"
   fi
 }
 export -f printStackTrace
-
-Exception::makeExceptionMsg(){
-  # Usage: Exception::makeException 'a' 'b' 'c' ....
-  local msg="Exception: $*"
-
-  # get exception info from cache file
-  if [ -n "$__boot_exception_cache" ]; then
-    local exceptionCache=$(cat $__boot_exception_cache)
-    if [[ -n "$exceptionCache" ]]; then
-      msg="$msg\n${exceptionCache}"
-    fi
-  fi
-
-  for (( i=0; i<${#BASH_SOURCE[@]}; i++));do
-    if [[ "${BASH_SOURCE[i]}" != *"lib/system/keyword/exception.sh" ]]; then
-      msg="${msg}\n    at ${BASH_SOURCE[i]} (${FUNCNAME[i]}:${BASH_LINENO[i - 1]})"
-    fi
-  done
-
-  echo "$msg"
-}
-export -f Exception::makeExceptionMsg
 
 throw() {
   # Usage: throw 'a' 'b' 'c' ....
@@ -55,9 +32,9 @@ throw() {
   # make error
   if [ ! -z "$___in_try_catch___" ]; then
     # handler try...catch...
-    Exception::makeExceptionMsg $* >> "$__boot_exception_cache"
-
-    # echo "inner___EXCEPTION___=$___EXCEPTION___"
+    # cache message of throw by ___EXCEPTION___
+    # this cache will be used and setted full message in `catch`
+    ___EXCEPTION___="$*"
     exit 255
   else
     # if not in try..catch, kill caller
@@ -71,17 +48,56 @@ throw() {
 }
 export -f throw
 
-Exception::GetException(){
-  # Usage: Exception::GetException 'lastCommandExitCode'
-  if [ $1 -ne 255 ]; then
-    if [ $1 -eq 127 ]; then
-      Exception::makeExceptionMsg "exit 127; Command not find"
+# only be called by addTrap
+__getErrorInfoInTrap(){
+  if [ $exitCode -eq 0 ]; then
+    return
+  fi
+
+  if [ $exitCode -ne 255 ]; then
+    if [ $exitCode -eq 127 ]; then
+      Exception::makeExceptionMsg "exitCode=127; Command not find" >> "$__boot_exception_cache"
     else
-      Exception::makeExceptionMsg "exit $1;"
+      Exception::makeExceptionMsg "exitCode=$exitCode" >> "$__boot_exception_cache"
     fi
   else
-    cat "$__boot_exception_cache"
-    > "$__boot_exception_cache"
+    Exception::makeExceptionMsg "$___EXCEPTION___" >> "$__boot_exception_cache"
   fi
+}
+export -f __getErrorInfoInTrap
+
+Exception::makeExceptionMsg(){
+  # Usage: Exception::makeException 'a' 'b' 'c' ....
+  local msg="Exception: $*"
+
+  # get exception info from cache file
+  # if [ -n "$__boot_exception_cache" ]; then
+  #   local exceptionCache=$(cat $__boot_exception_cache)
+  #   if [[ -n "$exceptionCache" ]]; then
+  #     msg="$msg\n${exceptionCache}"
+  #   fi
+  # fi
+
+  # i from 1, skip itself
+  # must skip:
+  #     1. lib/system/keyword/exception.sh
+  #     2. lib/system/builtin/method.sh
+  local skip='.*lib/system/(keyword/exception.sh|builtin/method.sh)$'
+  for (( i=1; i<${#BASH_SOURCE[@]}; i++));do
+    if [[ "${BASH_SOURCE[i]}" =~ $skip ]]; then
+      continue
+    else
+      msg="${msg}\n    at ${BASH_SOURCE[i]} (${FUNCNAME[i]}:${BASH_LINENO[i - 1]})"
+    fi
+  done
+
+  echo "$msg"
+}
+export -f Exception::makeExceptionMsg
+
+Exception::GetException(){
+  # Usage: Exception::GetException 'lastCommandExitCode'
+  cat "$__boot_exception_cache"
+  > "$__boot_exception_cache"
 }
 export -f Exception::GetException
--- test/lib/system/throw3.sh
+++ test/lib/system/throw3.sh
@@ -6,29 +6,11 @@
 source $( cd $([[ "${BASH_SOURCE[0]}" != *"/"* ]] && echo "." || echo "${BASH_SOURCE[0]%/*}"); pwd )/../../../boot.sh
 
 try {
-  try {
-    echo '001'
-    throw 'error--1'
-  } catch {
-    # echo 'catch'
-    printStackTrace "$___EXCEPTION___"
-  }
-
-  echo 002
-  exit 234
-  echo 003
+  echo 111
+  exit 255
+  echo 222
 } catch {
+  # echo 'catch'
   printStackTrace "$___EXCEPTION___"
 }
-echo 004
-
-throw 'error--2'
-
-echo 005
-
-# 001
-# error--1
-# 002
-# error--exit 234
-# 004
-# error--2
+echo 333
